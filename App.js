2613fa9401696ec7ccf665bb2fa41e2b321b06b1
Ur Father <fred.geffray@gmail.com>
2020-05-28 15:11:07 +0200

    update

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Salut ça va ?</Text>
     </View>
   );
 }
@@ -12,8 +12,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#191919',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: '#fff',
+    fontSize: 22,
+    padding: 20,
+    backgroundColor: '#3c3c3c',
+    borderRadius: 10,
+  }
 });
