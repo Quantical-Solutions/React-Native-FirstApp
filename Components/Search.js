7cdf5e0b49811b05614a93c86bc7509b1c7fcdc0
Fred Geffray <fred.geffray@gmail.com>
2020-05-29 12:42:51 +0200

    update

--- Components/Search.js
+++ Components/Search.js
@@ -1,7 +1,7 @@
 // Components/Search.js
 
 import React from 'react'
-import { StyleSheet, View, TextInput, Button, Text, FlatList } from 'react-native'
+import { StyleSheet, View, TextInput, Button, FlatList, ActivityIndicator } from 'react-native'
 import FilmItem from './FilmItem'
 import { getFilmsFromApiWithSearchedText } from '../API/TMDBApi'
 
@@ -10,17 +10,48 @@ class Search extends React.Component {
     constructor(props) {
         super(props)
         this.searchedText = "" // Initialisation de notre donnée searchedText en dehors du state
+        this.page = 0
+        this.totalPages = 0
         this.state = {
-            films: []
+            films: [],
+            isLoading: false
+        }
+    }
+
+    _displayLoading() {
+        if (this.state.isLoading) {
+            return (
+                <View style={styles.loading_container}>
+                    <ActivityIndicator size="large"/>
+                </View>
+            )
         }
     }
 
     _loadFilms() {
+        this.setState({ isLoading: true })
         if (this.searchedText.length > 0) { // Seulement si le texte recherché n'est pas vide
-            getFilmsFromApiWithSearchedText(this.searchedText).then(data => {
-                this.setState({ films: data.results })
+            getFilmsFromApiWithSearchedText(this.searchedText, this.page+1).then(data => {
+                this.page = data.page
+                this.totalPage = data.total_pages
+                this.setState({
+                    films: [ ...this.state.films, ...data.results],// concatenation des deux tableaux en les copiant
+                    // grâce aux spreads
+                    isLoading: false
+                })
             })
         }
+    }
+
+    _searchFilms() {
+        this.page = 0
+        this.totalPage = 0
+        this.setState({
+            films: []
+        }, () => {
+            console.log("Page: " + this.page + ", Total: " + this.totalPage + ", Nombre de films: " + this.state.films.length)
+            this._loadFilms()
+        })
     }
 
     _searchTextInputChanged(text) {
@@ -34,14 +65,22 @@ class Search extends React.Component {
                 <TextInput
                     style={styles.textinput}
                     placeholder='Titre du film'
+                    onSubmitEditing={() => this._searchFilms()}
                     onChangeText={(text) => this._searchTextInputChanged(text)}
                 />
-                <Button title='Rechercher' onPress={() => this._loadFilms()}/>
+                <Button title='Rechercher' onPress={() => this._searchFilms()}/>
                 <FlatList
                     data={this.state.films}
+                    onEndReachedTreshold={0.5}
+                    onEndReached={() => {
+                        if (this.page < this.totalPage) {
+                            this._loadFilms()
+                        }
+                    }}
                     keyExtractor={(item) => item.id.toString()}
                     renderItem={({item}) => <FilmItem film={item}/>}
                 />
+                {this._displayLoading()}
             </View>
         )
     }
@@ -59,6 +98,15 @@ const styles = StyleSheet.create({
         borderColor: '#000000',
         borderWidth: 1,
         paddingLeft: 5
+    },
+    loading_container: {
+        position: 'absolute',
+        top: 100,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        alignItems: 'center',
+        justifyContent: 'center'
     }
 })
 
