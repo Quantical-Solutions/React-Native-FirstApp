df5ccc7d0a67ceeacde007b4e3625c228b7654ea
Fred Geffray <fred.geffray@gmail.com>
2020-05-29 13:36:22 +0200

    update

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import Search from './Components/Search'
+import React from 'react'
+import Navigation from './Navigation/Navigation'
 
 export default class App extends React.Component {
 
   render() {
     return (
-        <Search />
+        <Navigation />
     );
   }
 }
